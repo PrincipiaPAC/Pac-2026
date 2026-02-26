c7c30fcda7dea45d644fa0d6414fe4f7d3c8f766
Kesiena Berezi <berezikesiena@gmail.com>
2026-02-26 15:13:13 -0600

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
 export const config = {
-  googleSheetScriptUrl: "https://script.google.com/a/macros/principia.edu/s/AKfycbzH5PQ-VIoMHeoLM03ZscxzEbfhpsT-kQfIG7W2RPMemsbnsEUrSHPc8NyWR54Nw0E4/exec"
+  googleSheetScriptUrl: "hhttps://script.google.com/a/macros/principia.edu/s/AKfycbyJ0uPjstRA64q0iIM6oDEaeopgypgO7EdE_Rf7k2j8pBut3ZCGpjhYsowE-2mCefIT/exec"
 }
